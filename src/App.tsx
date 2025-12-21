01efada515c05b2ae995371a3eabc075ca4a3d4f
davigold <davi.goldwasser@gmail.com>
2025-12-21 09:35:43 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import FullApp from './pages/FullApp';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { DataProvider } from './contexts/DataContext';
+import ErrorBoundary from './components/ErrorBoundary';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <DataProvider>
-          <FullApp />
-        </DataProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <ThemeProvider>
+          <DataProvider>
+            <FullApp />
+          </DataProvider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
